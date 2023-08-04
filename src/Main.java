4961b546cc75f0f10c2d433e6b85742852c6767c
Daniel Magdziarz <127679184+Daniel-Magdziarz@users.noreply.github.com>
2023-08-04 15:25:23 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 /*This program calculates the amount of buckets needed to paint a wall. It features an interactive menu,
-.hasNextInt, Enhanced Switch & stripIndent (Java 14 and up), adn method overloading.
+.hasNextInt, Enhanced Switch & stripIndent (Java 14 and up), try-catch, and method overloading.
 This was written in IntelliJ with Java 17.*/
 //Author: Daniel Magdziarz
 
